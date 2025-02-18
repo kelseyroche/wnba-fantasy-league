900e3a45b78cefd8a161d9ca230d7738ac33385e
Kelsey Roche <roche.kelsey.24@gmail.com>
2025-02-18 12:54:34 -0500

    trying to fix front end for rendeR

--- client/src/components/leaderboard/Leaderboard.jsx
+++ client/src/components/leaderboard/Leaderboard.jsx
@@ -1,58 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { Table, Header } from 'semantic-ui-react'; 
-// import Navbar from '../common/NavBar';
-// import Footer from '../common/Footer';
-// import './Leaderboard.css';
-
-// const Leaderboard = () => {
-//   const [leaderboard, setLeaderboard] = useState([]);
-
-//   useEffect(() => {
-//     const fetchLeaderboard = async () => {
-//       try {
-//         const response = await axios.get('http://localhost:5555/leaderboard');
-
-//         const sortedLeaderboard = response.data.sort((a, b) => b.season_score - a.season_score);
-
-//         setLeaderboard(sortedLeaderboard);
-//       } catch (error) {
-//         console.error('Error fetching leaderboard data:', error);
-//       }
-//     };
-
-//     fetchLeaderboard();
-//   }, []);
-
-//   return (
-//     <div className="page-container"> 
-//       <Navbar />
-//       <div className="centered-container">
-//         <Header as="h2">Leaderboard</Header>
-//         <Table celled className="centered-table">
-//           <Table.Header>
-//             <Table.Row>
-//               <Table.HeaderCell>Username</Table.HeaderCell>
-//               <Table.HeaderCell>Season Score</Table.HeaderCell>
-//             </Table.Row>
-//           </Table.Header>
-//           <Table.Body>
-//             {leaderboard.map((entry, index) => (
-//               <Table.Row key={index}>
-//                 <Table.Cell>{entry.username}</Table.Cell>
-//                 <Table.Cell>{entry.season_score}</Table.Cell>
-//               </Table.Row>
-//             ))}
-//           </Table.Body>
-//         </Table>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Leaderboard;
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Table, Header } from 'semantic-ui-react'; 
@@ -67,7 +12,7 @@ const Leaderboard = () => {
   useEffect(() => {
     const fetchLeaderboard = async () => {
       try {
-        const response = await axios.get('${import.meta.env.VITE_API_URL}/leaderboard');
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/leaderboard`);
         const sortedLeaderboard = response.data.sort((a, b) => b.season_score - a.season_score);
         setLeaderboard(sortedLeaderboard);
       } catch (error) {
